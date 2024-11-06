e52fc0042f9bf52cf25587fffbe7ac043aaaa651
chenjiakai004 <chenjiakai004@ke.com>
2024-11-06 18:44:40 +0800

    init

---
+++ api/src/main/java/com/ke/bella/files/Application.java
@@ -0,0 +1,20 @@
+package com.ke.bella.files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.ke.bella.openapi.login.config.EnableBellaLogin;
+
+/**
+ * 服务启动类
+ *
+ * @author keboot
+ */
+@EnableApolloConfig
+@SpringBootApplication(scanBasePackages = { "com.ke.bella.files" })
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
