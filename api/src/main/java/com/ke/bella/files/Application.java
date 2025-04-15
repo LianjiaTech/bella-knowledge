7c6ca94012530496d4049f4f5cc26d0977942b51
chenjiakai004 <chenjiakai004@ke.com>
2025-04-15 11:48:07 +0800

    fix: create temp file dir

--- api/src/main/java/com/ke/bella/files/Application.java
+++ api/src/main/java/com/ke/bella/files/Application.java
@@ -1,5 +1,6 @@
 package com.ke.bella.files;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.ke.bella.files.configuration.ApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(ApolloConfig.class)
 @SpringBootApplication(scanBasePackages = { "com.ke.bella.files" })
+@EnableApolloConfig
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
